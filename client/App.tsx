fda901224839caa7df17da35e1f89f9df4a6cd9e
Hezekiel Albi Sidauruk <222212644@stis.ac.id>
2025-09-24 09:50:09 +0700

    commit kedua

--- client/App.tsx
+++ client/App.tsx
@@ -5,12 +5,8 @@ import { createRoot } from "react-dom/client";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import { BrowserRouter } from "react-router-dom";
 import AIAssistant from "./pages/AIAssistant";
-import FAQ from "./pages/FAQ";
-import Tentang from "./pages/Tentang";
-import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -20,14 +16,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/ai-assistant" element={<AIAssistant />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/tentang" element={<Tentang />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AIAssistant />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
